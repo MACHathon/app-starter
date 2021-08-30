2914fb10972ed1767b91dd47de0de4ad69628d2a
Dave Leigh <david.leigh@valtech.com>
2021-08-30 13:21:11 +0100

    Title

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <Head>
-        <title>Toykens!</title>
+        <title>Welcome to the Toyken ecosystem | Children helping businesses through economic recovery</title>
         <meta name="description" content="Toyken" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
